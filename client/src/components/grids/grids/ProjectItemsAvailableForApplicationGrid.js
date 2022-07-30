4978946c789056672916a01c2891c3af6e8e94f4
developer <i9i9i9m>
2022-07-30 21:14:03 +0000

    nununn

--- client/src/components/grids/grids/ProjectItemsAvailableForApplicationGrid.js
+++ client/src/components/grids/grids/ProjectItemsAvailableForApplicationGrid.js
@@ -1,72 +1,111 @@
+/** @format */
+
 import React from 'react';
-import {AgGridReact} from "ag-grid-react";
-import {gridSelectionsVar} from "../../../cache";
+import { AgGridReact } from 'ag-grid-react';
+import { gridSelectionsVar } from '../../../cache';
+import { usePrevious } from 'react-use';
+import { formatNumberGridTwoDecimals } from '../../../functions/formattingFunctions';
 
-const ProjectItemsAvailableForApplicationGrid = ({rowData}) => {
-    const gridRef = React.useRef()
-    const columnDefs = React.useMemo(() => [
-        {
-            field: 'worksheetReference'
-        },
-        {
-            field: 'itemNumber'
-        },
-        {
-            field: 'activityCode'
-        },
-        {
-            field: 'activityDescription'
-        },
-        {
-            field: 'qtyComplete'
-        },
-        {
-            field: 'unitPayableTotal'
-        },
-        {
-            field: 'valueComplete'
-        }
-    ], [])
+const ProjectItemsAvailableForApplicationGrid = ({ rowData }) => {
+	const prevRowData = usePrevious(rowData);
+	const gridRef = React.useRef();
+	const columnDefs = React.useMemo(
+		() => [
+			{
+				field: 'worksheetReference',
+				checkboxSelection: true,
+				headerCheckboxSelection: true,
+			},
+			{
+				field: 'itemNumber',
+			},
+			{
+				field: 'activityCode',
+			},
+			{
+				field: 'activityDescription',
+			},
+			{
+				field: 'qtyComplete',
+				valueFormatter: formatNumberGridTwoDecimals,
+				type: 'numericColumn',
+				filter: 'agNumberColumnFilter',
+			},
+			{
+				field: 'unitPayableTotal',
+				valueFormatter: formatNumberGridTwoDecimals,
+				type: 'numericColumn',
+				filter: 'agNumberColumnFilter',
+			},
+			{
+				field: 'valueComplete',
+				valueFormatter: formatNumberGridTwoDecimals,
+				type: 'numericColumn',
+				filter: 'agNumberColumnFilter',
+			},
+		],
+		[],
+	);
 
-    const defaultColDef = React.useMemo(
-        () => ({
-            filter: true,
-            sortable: true,
-            resizable: true,
-            flex: 1,
-        }),
-        [],
-    );
+	const defaultColDef = React.useMemo(
+		() => ({
+			filter: true,
+			sortable: true,
+			resizable: true,
+			flex: 1,
+		}),
+		[],
+	);
 
-    const onSelectionChanged = React.useCallback(() => {
-        const selectedRow = gridRef.current.api.getSelectedRows();
-        selectedRow.length === 0
-            ? gridSelectionsVar({...gridSelectionsVar(), selectedItem: false})
-            : gridSelectionsVar({
-                ...gridSelectionsVar(),
-                selectedItem: selectedRow,
-            });
-    }, []);
+	console.log(rowData, prevRowData);
 
-    return (
-        <AgGridReact
-            className='ag-theme-alpine'
-            animateRows='true'
-            columnDefs={columnDefs}
-            defaultColDef={defaultColDef}
-            rowData={rowData}
-            domLayout='autoHeight'
-            pagination={true}
-            paginationPageSize={20}
-            suppressRowClickSelection={true}
-            rowSelection={'multiple'}
-            onSelectionChanged={onSelectionChanged}
-            ref={gridRef}
-            overlayNoRowsTemplate={
-                '<span style="padding: 10px; border: 2px solid #444; background: lightgoldenrodyellow">Please Select An Available Location</span>'
-            }
-        />
-    );
+	const onSelectionChanged = React.useCallback(() => {
+		const selectedRow = gridRef.current.api.getSelectedRows();
+		selectedRow.length === 0
+			? gridSelectionsVar({ ...gridSelectionsVar(), selectedItem: false })
+			: gridSelectionsVar({
+					...gridSelectionsVar(),
+					selectedItem: selectedRow,
+			  });
+	}, []);
+
+	React.useEffect(() => {
+		if (gridRef.current.api) {
+			let selectedNodes = gridRef.current.api
+				.getSelectedNodes()
+				.map((item) => item.data.id);
+			console.log(selectedNodes);
+			const newRows = rowData
+				.filter((md) => prevRowData.every((fd) => fd.id !== md.id))
+				.map((item) => item.id);
+			const allSelected = [...selectedNodes, ...newRows];
+			gridRef.current.api.forEachNode((node) => {
+				node.setSelected(allSelected.includes(node.data.id));
+			});
+			selectedNodes = gridRef.current.api.getSelectedNodes();
+		}
+	}, [rowData]);
+
+	return (
+		<AgGridReact
+			className='ag-theme-alpine'
+			animateRows='true'
+			columnDefs={columnDefs}
+			defaultColDef={defaultColDef}
+			rowData={rowData}
+			getRowId={(params) => params.data.id}
+			domLayout='autoHeight'
+			pagination={true}
+			paginationPageSize={20}
+			suppressRowClickSelection={true}
+			rowSelection={'multiple'}
+			onSelectionChanged={onSelectionChanged}
+			ref={gridRef}
+			overlayNoRowsTemplate={
+				'<span style="padding: 10px; border: 2px solid #444; background: lightgoldenrodyellow">Please Select An Available Location</span>'
+			}
+		/>
+	);
 };
 
 export default ProjectItemsAvailableForApplicationGrid;
--- client/src/components/grids/grids/ProjectLocationsAvailableForApplicationGrid.js
+++ client/src/components/grids/grids/ProjectLocationsAvailableForApplicationGrid.js
@@ -1,91 +1,90 @@
 /** @format */
 
 import React from 'react';
-import {AgGridReact} from 'ag-grid-react';
-import {useQuery, useReactiveVar} from '@apollo/client';
-import {GET_PROJECT_ITEMS_AVAILABLE_FOR_APPLICATION} from '../../../api-calls/queries/applications';
-import {useParams} from 'react-router-dom';
-import {formatNumberGridTwoDecimals} from '../../../functions/formattingFunctions';
-import {gridSelectionsVar} from '../../../cache';
+import { AgGridReact } from 'ag-grid-react';
+import { useQuery, useReactiveVar } from '@apollo/client';
+import { GET_PROJECT_ITEMS_AVAILABLE_FOR_APPLICATION } from '../../../api-calls/queries/applications';
+import { useParams } from 'react-router-dom';
+import { formatNumberGridTwoDecimals } from '../../../functions/formattingFunctions';
+import { gridSelectionsVar } from '../../../cache';
 
-const ProjectLocationsAvailableForApplicationGrid = ({setItemData}) => {
-    const {id} = useParams();
-    const gridRef = React.useRef();
-    const {selectedLocation} = useReactiveVar(gridSelectionsVar);
-    const [rowData, setRowData] = React.useState([]);
-    const {data: itemData, loading} = useQuery(GET_PROJECT_ITEMS_AVAILABLE_FOR_APPLICATION, {
-        variables: {orderId: Number(id)},
-        onCompleted: (data) =>
-            setRowData(data.wpmGraphqlGetLocationsAvailableForApplication.nodes),
-    });
+const ProjectLocationsAvailableForApplicationGrid = ({ setItemData }) => {
+	const { id } = useParams();
+	const gridRef = React.useRef();
+	const { selectedLocation } = useReactiveVar(gridSelectionsVar);
+	const [rowData, setRowData] = React.useState([]);
+	const { data: itemData, loading } = useQuery(
+		GET_PROJECT_ITEMS_AVAILABLE_FOR_APPLICATION,
+		{
+			variables: { orderId: Number(id) },
+			onCompleted: (data) =>
+				setRowData(data.wpmGraphqlGetLocationsAvailableForApplication.nodes),
+		},
+	);
 
-    const columnDefs = React.useMemo(
-        () => [
-            {
-                field: 'worksheetReference',
-                checkboxSelection: true,
-            },
-            {
-                field: 'reference',
-            },
-            {
-                field: 'itemsComplete',
-                headerName: 'items Available',
-                type: 'numericColumn',
-            },
-            {
-                field: 'valueComplete',
-                headerName: 'Value Available',
-                valueFormatter: formatNumberGridTwoDecimals,
-                type: 'numericColumn',
-                filter: 'agNumberColumnFilter',
-            },
-        ],
-        [],
-    );
+	const columnDefs = React.useMemo(
+		() => [
+			{
+				field: 'worksheetReference',
+				checkboxSelection: true,
+				headerCheckboxSelection: true,
+			},
+			{
+				field: 'reference',
+			},
+			{
+				field: 'itemsComplete',
+				headerName: 'items Available',
+				type: 'numericColumn',
+			},
+			{
+				field: 'valueComplete',
+				headerName: 'Value Available',
+				valueFormatter: formatNumberGridTwoDecimals,
+				type: 'numericColumn',
+				filter: 'agNumberColumnFilter',
+			},
+		],
+		[],
+	);
 
-    const defaultColDef = React.useMemo(
-        () => ({
-            filter: true,
-            sortable: true,
-            resizable: true,
-            flex: 1,
-        }),
-        [],
-    );
+	const defaultColDef = React.useMemo(
+		() => ({
+			filter: true,
+			sortable: true,
+			resizable: true,
+			flex: 1,
+		}),
+		[],
+	);
 
-    const onSelectionChanged = React.useCallback(() => {
-        const selectedRow = gridRef.current.api.getSelectedRows();
-        selectedRow.length === 0
-            ? gridSelectionsVar({...gridSelectionsVar(), selectedLocation: false})
-            : gridSelectionsVar({
-                ...gridSelectionsVar(),
-                selectedLocation: selectedRow,
-            });
+	const onSelectionChanged = React.useCallback(() => {
+		const selectedRow = gridRef.current.api.getSelectedRows();
 
-        setItemData(itemData.wpmGraphqlGetItemsAvailableForApplication.nodes.filter(f => selectedRow.some(item => item.id === f.sitelocationId)))
+		setItemData(
+			itemData.wpmGraphqlGetItemsAvailableForApplication.nodes.filter((f) =>
+				selectedRow.some((item) => item.id === f.sitelocationId),
+			),
+		);
+	}, []);
 
-    }, []);
-
-    console.log(selectedLocation);
-
-    if (loading) return null;
-    return (
-        <AgGridReact
-            className='ag-theme-alpine'
-            animateRows='true'
-            columnDefs={columnDefs}
-            defaultColDef={defaultColDef}
-            rowData={rowData}
-            domLayout='autoHeight'
-            pagination={true}
-            paginationPageSize={20}
-            suppressRowClickSelection={true}
-            rowSelection={'multiple'}
-            onSelectionChanged={onSelectionChanged}
-            ref={gridRef}
-        />
-    );
+	if (loading) return null;
+	return (
+		<AgGridReact
+			className='ag-theme-alpine'
+			animateRows='true'
+			columnDefs={columnDefs}
+			defaultColDef={defaultColDef}
+			rowData={rowData}
+			domLayout='autoHeight'
+			pagination={true}
+			paginationPageSize={20}
+			suppressRowClickSelection={true}
+			rowSelection={'multiple'}
+			onSelectionChanged={onSelectionChanged}
+			ref={gridRef}
+		/>
+	);
 };
 
 export default ProjectLocationsAvailableForApplicationGrid;
